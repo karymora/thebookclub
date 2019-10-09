37b2d1d7ba059cf1e83c1b839ed1bbba8ac61243
Kary Mora <KaryMo@Karys-MacBook-Pro-2.local>
2019-10-08 21:15:14 -0500

    meeting details on the works

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,19 +3,19 @@ const router = express.Router()
 const User = require('../models/User')
 const Book = require('../models/Book')
 
-// router.get('/allusers', (req, res, next) => {
-//   User.find()
-//     .populate('booksRead', 'title')
-//     .then(user => res.status(200).json({ user }))
-//     .catch(error => res.status(500).json({ error }))
-// })
+router.get('/allusers', (req, res, next) => {
+  User.find()
+    .populate('booksRead', 'title')
+    .then(user => res.status(200).json({ user }))
+    .catch(error => res.status(500).json({ error }))
+})
 
-// router.get('/allusers/:id', async (req, res, next) => {
-//   const librosUser = User.findById(req.params.id)
-//     .populate({ path: 'booksRead', populate: { path: 'booksRead' } })
-//     .then(librosUser => res.status(200).json({ librosUser }))
-//     .catch(error => res.status(500).json({ error }))
-// })
+router.get('/allusers/:id', async (req, res, next) => {
+  const librosUser = User.findById(req.params.id)
+    .populate({ path: 'booksRead', populate: { path: 'booksRead' } })
+    .then(librosUser => res.status(200).json({ librosUser }))
+    .catch(error => res.status(500).json({ error }))
+})
 
 router.put('/allusers/:id', (req, res, next) => {
   User.findByIdAndUpdate(req.params.id, { ...req.body }, { new: true })
